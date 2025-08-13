a85e494f1e08ffc409102cf67294c50434d1ee58
finn <bishoe01@gmail.com>
2025-08-13 17:21:46 +0900

    refactor : view-viewmodel분리

---
+++ c5/c5/ViewModel/SearchViewModel.swift
@@ -0,0 +1,41 @@
+//
+//  SearchViewModel.swift
+//  c5
+//
+//  Created by bishoe01 on 8/13/25.
+//
+
+import SwiftUI
+
+@MainActor
+class SearchViewModel: ObservableObject {
+    @Published var searchText: String = ""
+    @Published var result: Pokemon? = nil
+
+    func searchAction(_ query: String) async {
+        let spec = APIEndpoint(url: "\(BASEURL)/\(query)",
+                               method: .get)
+        guard let url = URL(string: spec.url) else {
+            return
+        }
+        do {
+            var request = URLRequest(url: url)
+            request.httpMethod = spec.method.rawValue.uppercased()
+            let (data, _) = try await URLSession.shared.data(for: request)
+            let pokemon = try JSONDecoder().decode(SearchResultItem.self, from: data)
+
+            let item = Pokemon(
+                id: pokemon.id,
+                name: pokemon.name,
+                types: pokemon.types.map { $0.type.name },
+                frontImage: pokemon.sprites.front_default,
+                backImage: pokemon.sprites.back_default
+            )
+
+            result = item
+            // print(item)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+}
--- c5/c5/Views/ContentView.swift
+++ c5/c5/Views/ContentView.swift
@@ -8,46 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var searchText: String = ""
-    @State private var result: Pokemon? = nil
-
-    // 검색 결과
-
-    func searchAction(_ query: String) async {
-        let spec = APIEndpoint(url: "\(BASEURL)/\(query)",
-                               method: .get)
-        guard let url = URL(string: spec.url) else {
-            return
-        }
-        do {
-            var request = URLRequest(url: url)
-            request.httpMethod = spec.method.rawValue.uppercased()
-            let (data, _) = try await URLSession.shared.data(for: request)
-            let pokemon = try JSONDecoder().decode(SearchResultItem.self, from: data)
-
-            let item = Pokemon(
-                id: pokemon.id,
-                name: pokemon.name,
-                types: pokemon.types.map { $0.type.name },
-                frontImage: pokemon.sprites.front_default,
-                backImage: pokemon.sprites.back_default
-            )
-            result = item
-            print(item)
-        } catch {
-            print(error.localizedDescription)
-        }
-    }
+    @StateObject var vm = SearchViewModel()
 
     var body: some View {
         VStack {
-            TextField("이름", text: $searchText)
+            TextField("이름", text: $vm.searchText)
                 .onSubmit {
-                    let q = searchText
+                    let q = vm.searchText
                     print(q)
-                    Task { await searchAction(q) }
+                    Task { await vm.searchAction(q) }
                 }
-            if let result {
+            if let result = vm.result {
                 VStack {
                     Text(result.name)
 
